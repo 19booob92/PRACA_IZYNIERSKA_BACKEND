00450ac55430492dae59a9e27cd07efca4d1ee01
19booob92 <19mateusz92@gmail.com>
2014-11-13 20:36:04 +0100

    poprawiono zaznaczanie radio buttonów w edycji pytań

--- src/main/java/sample/data/jpa/utils/Courses.java
+++ src/main/java/sample/data/jpa/utils/Courses.java
@@ -4,4 +4,7 @@ package sample.data.jpa.utils;
 public interface Courses {
     final static String ALGEBRA = "Algebra";
     final static String ANALIZA = "Analiza";
+    final static String INFORMATYKA = "Informatyka";
+    final static String PROGRAMOWNAIE = "Programowanie";
+    final static String FIZYKA = "Fizyka";
 }
